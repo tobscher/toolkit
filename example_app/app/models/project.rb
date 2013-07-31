0eba7b2a8100d80c4e9da22236625f11578516a4
Tobias Haar <tobias.haar@gmail.com>
2013-07-31 14:03:17 +0100

    example code

--- example_app/app/models/project.rb
+++ example_app/app/models/project.rb
@@ -1,9 +1,9 @@
 class Project < ActiveRecord::Base
-  toolkit :assignable, :reportable
+  toolkit :assignable, :reportable#,
     #:reportable_options => { :burndown => false, :forecast => true },
     #:assignable_options => { :name_present => true }
 
   # def burndown
-  #   p "My burn down method (which is better anyway)"
+  #  p "My burn down method (which is better anyway)"
   # end
 end
